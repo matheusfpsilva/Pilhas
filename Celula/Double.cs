b14a098d5402b8c0c6d23fb0ef040468267ce150
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:11:29 -0300

    Create Double.cs

---
+++ Celula/Double.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilha-Celula
+{
+    internal class Double
+    {
+        private double valor;
+        public double Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+        public Double(int valor)
+        {
+            this.valor = valor;
+        }
+        public Double()
+        {
+            valor = 0;
+        }
+    }
+}
