1c8f987d6bb65751ef79beac90aa360b7e5c5ee5
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:10:00 -0300

    Create ClassePilha-1.cs

---
+++ Vetor/ClassePilha-1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilha-Vetor
+{
+    internal class Pilha
+    {
+        private double[] pilha;
+        private int topo;
+        public Pilha(int tam)
+        {
+            pilha = new double[tam];
+            topo = 0;
+        }
+        public void Empilhar(double valor)
+        {
+            if (topo < pilha.Length)
+            {
+                pilha[topo] = valor;
+                topo++;
+            }
+            else
+            {
+                Console.WriteLine("Pilha cheia!");
+            }
+        }
+        public double Desempilhar()
+        {
+            if (topo > 0)
+            {
+                topo--;
+                return pilha[topo];
+            }
+            else
+            {
+                throw new Exception("Pilha vazia!");
+            }
+        }
+
+    }
+}
