cfe1c70da39da71c69d39c6caa2a30c254f2291a
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:07:52 -0300

    Create Stack2.cs

---
+++ Stack/Stack2.cs
@@ -0,0 +1,39 @@
+﻿ using System;
+using System.Collections.Generic;
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> pilha = new Stack<char>();
+            Console.Write("Informe a sequência: ");
+            string sequencia = Console.ReadLine();
+            for (int i = 0; i < sequencia.Length; i++)
+            {
+                char c = sequencia[i];
+                if (c == ')' || c == ']')
+                {
+                    if (pilha.Count == 0 || (c == ')' && pilha.Peek() != '(') || (c == ']' && pilha.Peek() != '['))
+                    {
+                        return;
+                    }
+                    pilha.Pop();
+                }
+                else if (c == '(' || c == '[')
+                {
+                    pilha.Push(c);
+                }
+            }
+            if (pilha.Count == 0)
+            {
+                Console.WriteLine("sequência está bem-formada!");
+            }
+            else
+            {
+                Console.WriteLine("a sequência está malformada!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
