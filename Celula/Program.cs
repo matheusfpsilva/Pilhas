c57dd78ca5ebe120594010d97cf8b9ad94db920c
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:12:21 -0300

    Create Program.cs

---
+++ Celula/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Pilha-Celula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilha minhaPilha = new Pilha();
+            Console.Write("Informe a expressão: ");
+            string expressao = Console.ReadLine();
+            for (int i = 0; i < expressao.Length; i++)
+            {
+                if (expressao[i] != '+' && expressao[i] != '-' && expressao[i] != '*' && expressao[i] != '/')
+                {
+                    int numeros = (int)char.GetNumericValue(expressao[i]);
+                    Double exp = new Double(numeros);
+                    minhaPilha.empilhar(exp);
+                }
+                else
+                {
+                    char operador = expressao[i];
+                    Double num1 = minhaPilha.desempilhar();
+                    Double num2 = minhaPilha.desempilhar();
+                    Double res = new Double();
+                    if (operador == '+')
+                    {
+                        res.Valor = num1.Valor   + num2.Valor;
+                    }
+                    else if (operador == '-')
+                    {
+                        res.Valor = num2.Valor - num1.Valor;
+                    }
+                    else if (operador == '*')
+                    {
+                        res.Valor = num1.Valor  * num2.Valor;
+                    }
+                    else if (operador == '/')
+                    {
+                        if (num1.Valor == 0)
+                        {
+                            Console.WriteLine("erro");
+                        }
+                        res.Valor  = num2.Valor / num1.Valor;
+                    }
+                    minhaPilha.empilhar(res);
+                }
+            }
+            Double topo = minhaPilha.consultarTopo();
+            Console.WriteLine("Resposta: " + topo.Valor);
+            Console.ReadKey();           
+        }
+    }
+}
